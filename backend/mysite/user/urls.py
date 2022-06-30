74e16abd6caf953946bdd7d81b1c5fe816d69d53
sahar <saharkashany@gmail.com>
2022-07-01 00:49:32 +0430

    backing up the user auth and front-end of the project

---
+++ backend/mysite/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import Signup
+from django.contrib.auth import views as authViews
+
+urlpatterns = [
+    path('signup/', Signup, name='signup'),
+    path('login/', authViews.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', authViews.LogoutView.as_view(), {'next_page': 'home'}, name='logout'),
+]
---
+++ backend/mysite/user/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import SignupForm
+from django.contrib.auth.models import User
+
+
+def Signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+            User.objects.create_user(username=username, email=email, password=password)
+            messages.success(request, f'Account created for {username}!')
+            return redirect('home')
+    else:
+        form = SignupForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'signup.html', context)
+
+
